778860c25c487f81d7d4a91c73f037648d21707b
Jérémie 'Meier Link' Balagna-Ranin <jeremie.balagna@gmail.com>
2014-01-05 00:54:18 +0100

    comment executed insert clauses

--- src/fill_quizz.sql
+++ src/fill_quizz.sql
@@ -55,74 +55,74 @@
 --("What is the octal value for the chmod command to set a file so that it can be read and written to by the owner, and read by the group and all other users? It should not be executable.", 1, 2);
 --INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (8, 1, "644"), (8, 0, "755"), (8, 0, "664"), (8, 0, "640");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("La commande ________ permet de lister les périphériques USB présents sur la machine.", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (9, 0, "lspci"), (9, 1, "lsusb"), (9, 0, "ls /dev");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("La commande ________ permet de lister les périphériques USB présents sur la machine.", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (9, 0, "lspci"), (9, 1, "lsusb"), (9, 0, "ls /dev");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("Comment peut-on afficher le contenu d'un fichier en sens inverse ?", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (10, 0, "cat -r"), (10, 0, "tail"), (10, 1, "tac"), (10, 0, "sort -r");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("Comment peut-on afficher le contenu d'un fichier en sens inverse ?", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (10, 0, "cat -r"), (10, 0, "tail"), (10, 1, "tac"), (10, 0, "sort -r");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("Le choix du périphérique de démarrage de la machine se fait", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
-(11, 1, "Dans le BIOS"), (11, 0, "Avec une option du chargeur de démarrage (boot loader)"), (11, 0, "Automatiquement");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("Le choix du périphérique de démarrage de la machine se fait", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
+--(11, 1, "Dans le BIOS"), (11, 0, "Avec une option du chargeur de démarrage (boot loader)"), (11, 0, "Automatiquement");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("Quels droits sont positionnés par la commande chmod 1777 un_repertoire sur un répertoire (plusieurs réponses) ?", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
-(12, 1, "Elle permet à tous les utilisateurs d'accéder au répertoire."),
-(12, 0, "Elle interdit l'accès aux autres utilisateurs que le groupe."),
-(12, 1, "Elle permet à tous de créer des fichiers dans ce répertoire, mais pas de modifier ou d'effacer ceux des autres.");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("Quels droits sont positionnés par la commande chmod 1777 un_repertoire sur un répertoire (plusieurs réponses) ?", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
+--(12, 1, "Elle permet à tous les utilisateurs d'accéder au répertoire."),
+--(12, 0, "Elle interdit l'accès aux autres utilisateurs que le groupe."),
+--(12, 1, "Elle permet à tous de créer des fichiers dans ce répertoire, mais pas de modifier ou d'effacer ceux des autres.");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("La commande locate", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
-(13, 0, "fonctionne comme find mais avec des options différentes."),
-(13, 1, "est plus efficace que find car elle utilise un index."),
-(13, 0, "permet de changer la langue par défaut de Linux.");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("La commande locate", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
+--(13, 0, "fonctionne comme find mais avec des options différentes."),
+--(13, 1, "est plus efficace que find car elle utilise un index."),
+--(13, 0, "permet de changer la langue par défaut de Linux.");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("Les droits d'un fichier sont « rw-r--r-- ». La valeur de umask est:", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (14, 0, "666"), (14, 1, "022"), (14, 0, "123"), (14, 1, "133");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("Les droits d'un fichier sont « rw-r--r-- ». La valeur de umask est:", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (14, 0, "666"), (14, 1, "022"), (14, 0, "123"), (14, 1, "133");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("Quelle différence y a-t-il entre les systèmes de fichiers ext2 et ext3 ?", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
-(15, 0, "ext3 est un nouveau système, incompatible avec ext2."),
-(15, 1, "ext3 est une simple extension de ext2 comprenant la journalisation."),
-(15, 1, "on peut passer de ext2 à ext3 et inversement très facilement.");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("Quelle différence y a-t-il entre les systèmes de fichiers ext2 et ext3 ?", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
+--(15, 0, "ext3 est un nouveau système, incompatible avec ext2."),
+--(15, 1, "ext3 est une simple extension de ext2 comprenant la journalisation."),
+--(15, 1, "on peut passer de ext2 à ext3 et inversement très facilement.");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("Quelle commande utiliser pour afficher les messages du noyau lors du démarrage du système ?", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (16, 0, "mess"), (16, 1, "dmesg"), (16, 0, "bootmsg"), (16, 0, "lsmsg");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("Quelle commande utiliser pour afficher les messages du noyau lors du démarrage du système ?", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (16, 0, "mess"), (16, 1, "dmesg"), (16, 0, "bootmsg"), (16, 0, "lsmsg");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("Quelle est la commande qui permet d'afficher toute les variables ?", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (17, 1, "set"), (17, 0, "varlist"), (17, 0, "vshow var"), (17, 0, "ps");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("Quelle est la commande qui permet d'afficher toute les variables ?", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES (17, 1, "set"), (17, 0, "varlist"), (17, 0, "vshow var"), (17, 0, "ps");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("Quelle commande permet d’effacer tous les fichiers et les sous-répertoires de l'arborescence /home/nicolas/truc/ ?", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
-(18, 0, "rm -r /home/nicolas/"), (18, 0, "rmdir /home/nicolas/truc"), (18, 1, "rm -r /home/nicolas/truc/*");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("Quelle commande permet d’effacer tous les fichiers et les sous-répertoires de l'arborescence /home/nicolas/truc/ ?", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
+--(18, 0, "rm -r /home/nicolas/"), (18, 0, "rmdir /home/nicolas/truc"), (18, 1, "rm -r /home/nicolas/truc/*");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("La commande pstree permet d’afficher", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
-(19, 1, "l'arborescence des processus par « famille »."),
-(19, 0, "uniquement les processus d’un utilisateur."),
-(19, 0, "l'occupation du processeur par les processus.");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("La commande pstree permet d’afficher", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
+--(19, 1, "l'arborescence des processus par « famille »."),
+--(19, 0, "uniquement les processus d’un utilisateur."),
+--(19, 0, "l'occupation du processeur par les processus.");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("La commande killall permet de", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
-(20, 0, "tuer tous les processus d'un utilisateur."),
-(20, 1, "tuer un processus sans connaître son PID."),
-(20, 0, "envoyer tous les signaux standard à un processus.");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("La commande killall permet de", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
+--(20, 0, "tuer tous les processus d'un utilisateur."),
+--(20, 1, "tuer un processus sans connaître son PID."),
+--(20, 0, "envoyer tous les signaux standard à un processus.");
 
-INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
-("La commande paste permet de", 1, 1);
-INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
-(21, 0, "coller un fichier à la suite d'un autre."),
-(21, 1, "joindre deux fichiers côte à côte simplement sans critères."),
-(21, 0, "laisser un processus en mémoire.");
+--INSERT INTO question (q_txt, q_lvl, q_lang) VALUES
+--("La commande paste permet de", 1, 1);
+--INSERT INTO answer (a_q_id, a_is_right, a_txt) VALUES
+--(21, 0, "coller un fichier à la suite d'un autre."),
+--(21, 1, "joindre deux fichiers côte à côte simplement sans critères."),
+--(21, 0, "laisser un processus en mémoire.");
